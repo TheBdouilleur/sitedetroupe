f77c9938059507d7518e5527eb0f0ddeb8b32717
TheBdouilleur <>
2021-04-11 16:19:44 +0000

    Reverted because idea was stupid

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 /*Dérouler et fermer la navbar quand l'utilisateur clique sur le bouton correspondant*/
 function Dérouler() {
-    var x = document.getElementsByClassName("navbar");
-    if (x.className === "navbar") 
+    var x = document.getElementById("navbar");
+    if (x.className === "topnav") {
       x.className += " responsive";
     } else {
-      x.className = "navbar";
+      x.className = "topnav";
     }
   } 
